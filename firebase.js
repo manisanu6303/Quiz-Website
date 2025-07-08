f5af12b2d67517217ec0a9a56174aeb3be7c2d70
Mani <manisanu9040@gmail.com>
2025-07-08 14:55:55 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCpAKZ3Z3rjVBqinm0uW-yZO7iIH7sNeGk",
+  authDomain: "quizappms.firebaseapp.com",
+  projectId: "quizappms",
+  storageBucket: "quizappms.appspot.com",
+  messagingSenderId: "619334780581",
+  appId: "1:619334780581:web:7e2c456372389fa2b7ef4c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
